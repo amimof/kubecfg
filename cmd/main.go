f21e5ac44782a12b5675574437bfc441eaad5a98
Amir Mofasser <amimof@users.noreply.github.com>
2023-03-21 20:25:16 +0100

    Adds warning prompt if config already exists (#2)
    * Adds warning prompt if config already exists
    
    * warningModel creates backup & symlink
    
    * Switch between kubeconfigs
    
    * If config file exists then a prompt is displayed where user can make choices
    * Selecting an item in the list of kubeconfigs will create a symlink to it

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	//"flag"
 
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/duration"
 
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
@@ -16,6 +22,15 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/pflag"
+)
+
+const (
+	VIEW_DEFAULT = iota
+	VIEW_CONFIG_EXISTS
+	VIEW_ERROR
+
+	stateShowDefault state = iota
+	stateShowWarning
 )
 
 var (
@@ -27,9 +42,9 @@ var (
 	BRANCH string
 	// GOVERSION used to compile. Is set when project is built and should never be set manually
 	GOVERSION string
-)
 
-var (
+	// Errors
+	ErrExist = errors.New("file already exists")
 
 	// Colors
 	subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
@@ -42,6 +57,11 @@ var (
 			Width(listPaneWidth).
 			Height(listPaneHeight).
 			PaddingTop(1)
+
+	errorStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("#d75f00")).
+			Foreground(lipgloss.Color("#eeeeee")).
+			MarginRight(2)
 
 	// The pane on the right that displays details about the currently selected file
 	detailsPane = lipgloss.NewStyle().
@@ -66,22 +86,36 @@ var (
 			PaddingBottom(1)
 )
 
-type Result struct {
-	items    []Item
+type state int
+
+// Top level app model
+type model struct {
+	state        state
+	warningModel warningModel
+	defaultModel defaultModel
+}
+
+type defaultModel struct {
+	items    []*Item
 	list     list.Model
 	selected int
+	errMsg   string
 }
 
 type Item struct {
-	config *api.Config
-	info   os.FileInfo
-	title  string
-	desc   string
-	raw    []byte
+	config     *api.Config
+	info       os.FileInfo
+	title      string
+	desc       string
+	isSelected bool
 }
 
 func (i Item) Title() string {
-	return i.title
+	s := i.title
+	if i.isSelected {
+		s = lipgloss.NewStyle().Foreground(lipgloss.Color("#87ffaf")).Render(i.title + " •")
+	}
+	return s
 }
 func (i Item) Description() string {
 	return i.desc
@@ -90,11 +124,24 @@ func (i Item) FilterValue() string {
 	return i.title
 }
 
-func (r *Result) Add(i Item) {
+func newDefaultModel() defaultModel {
+	return defaultModel{}
+}
+
+func New() *model {
+	r := &model{
+		state:        stateShowDefault,
+		defaultModel: newDefaultModel(),
+		warningModel: newWarningModel(),
+	}
+	return r
+}
+
+func (r *defaultModel) Add(i *Item) {
 	r.items = append(r.items, i)
 }
 
-func (r *Result) List() []list.Item {
+func (r *defaultModel) List() []list.Item {
 	var items []list.Item
 	for _, i := range r.items {
 		items = append(items, i)
@@ -102,11 +149,50 @@ func (r *Result) List() []list.Item {
 	return items
 }
 
-func (r *Result) Init() tea.Cmd {
+func (r defaultModel) Init() tea.Cmd {
 	return nil
 }
 
-func (r *Result) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (r *model) Init() tea.Cmd {
+	return nil
+}
+
+func (r *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := lipgloss.NewStyle().Margin(1, 2).GetFrameSize()
+		r.defaultModel.list.SetSize(msg.Width-h, msg.Height-v)
+		w := msg.Width - listPaneWidth - h
+		he := msg.Height - v
+		detailsPane = detailsPane.Width(w)
+		detailsPane = detailsPane.Height(he - 1)
+	case OverwriteConfigMsg:
+		r.state = stateShowDefault
+		newDetaultModel, cmd := r.defaultModel.update(msg)
+		r.defaultModel = newDetaultModel
+		cmds = append(cmds, cmd)
+
+	}
+
+	switch r.state {
+	case stateShowDefault:
+		newDetaultModel, cmd := r.defaultModel.update(msg)
+		r.defaultModel = newDetaultModel
+		cmds = append(cmds, cmd)
+	case stateShowWarning:
+		newWarningModel, cmd := r.warningModel.update(msg)
+		r.warningModel = newWarningModel
+		cmds = append(cmds, cmd)
+	}
+
+	return r, tea.Batch(cmds...)
+}
+
+func (r defaultModel) update(msg tea.Msg) (defaultModel, tea.Cmd) {
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
@@ -120,6 +206,20 @@ func (r *Result) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if r.selected > 0 {
 				r.selected--
 			}
+		case "enter":
+			p, err := parseArgs()
+			if err != nil {
+				r.errMsg = err.Error()
+			}
+			err = os.Remove(path.Join(p, "config"))
+			if err != nil {
+				r.errMsg = err.Error()
+			}
+			err = os.Symlink(path.Join(p, r.items[r.selected].info.Name()), path.Join(p, "config"))
+			if err != nil {
+				r.errMsg = err.Error()
+			}
+			r.selectItem(r.selected)
 		}
 	case tea.WindowSizeMsg:
 		h, v := lipgloss.NewStyle().Margin(1, 2).GetFrameSize()
@@ -129,20 +229,29 @@ func (r *Result) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		detailsPane = detailsPane.Width(w)
 		detailsPane = detailsPane.Height(he - 1)
 	}
-
-	var cmd tea.Cmd
-	r.list, cmd = r.list.Update(msg)
-	return r, cmd
+	l, cmd := r.list.Update(msg)
+	r.list = l
+	cmds = append(cmds, cmd)
+	return r, tea.Batch(cmds...)
 }
 
-func (r *Result) View() string {
+func (r *defaultModel) selectItem(i int) {
+	for _, i := range r.items {
+		i.isSelected = false
+	}
+	r.items[i].isSelected = true
+}
 
+func (r defaultModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return r.update(msg)
+}
+
+func (r defaultModel) View() string {
 	curContext := lipgloss.JoinVertical(
 		lipgloss.Top,
 		detailsHeader.Render("Current Context"),
 		detailsContent.Render(r.getCurrentContext()),
 	)
-
 	contextCount := lipgloss.JoinVertical(
 		lipgloss.Left,
 		detailsHeader.Render("Contexts"),
@@ -158,20 +267,17 @@ func (r *Result) View() string {
 		detailsHeader.Render("Users"),
 		detailsContent.Render(r.getUserCount()),
 	)
-
 	counts := lipgloss.JoinHorizontal(
 		lipgloss.Left,
 		contextCount,
 		clusterCount,
 		userCount,
 	)
-
 	modified := lipgloss.JoinVertical(
 		lipgloss.Left,
 		detailsHeader.Render("Last Modified"),
 		detailsContent.Render(r.getLastModified()),
 	)
-
 	sizeBytes := lipgloss.JoinVertical(
 		lipgloss.Left,
 		detailsHeader.Render("Size"),
@@ -184,33 +290,45 @@ func (r *Result) View() string {
 		curContext,
 		modified,
 		sizeBytes,
+		errorStyle.Render(r.errMsg),
 	)
-
 	return lipgloss.JoinHorizontal(lipgloss.Top, listPane.Render(r.list.View()), detailsPane.Render(details))
+}
+
+func (r *model) View() string {
+	var s string
+	switch r.state {
+	case stateShowWarning:
+		return r.warningModel.View()
+	case stateShowDefault:
+		return r.defaultModel.View()
+	}
+
+	return s
 
 }
 
-func (r *Result) getContextCount() string {
+func (r *defaultModel) getContextCount() string {
 	str := strconv.Itoa(len(r.items[r.selected].config.Contexts))
 	return str
 }
 
-func (r *Result) getClusterCount() string {
+func (r *defaultModel) getClusterCount() string {
 	str := strconv.Itoa(len(r.items[r.selected].config.Clusters))
 	return str
 }
 
-func (r *Result) getUserCount() string {
+func (r *defaultModel) getUserCount() string {
 	str := strconv.Itoa(len(r.items[r.selected].config.AuthInfos))
 	return str
 }
 
-func (r *Result) getCurrentContext() string {
+func (r *defaultModel) getCurrentContext() string {
 	return r.items[r.selected].config.CurrentContext
 }
 
-func (r *Result) getLastModified() string {
-	return r.items[r.selected].info.ModTime().String()
+func (r *defaultModel) getLastModified() string {
+	return duration.HumanDuration(time.Since(r.items[r.selected].info.ModTime())) + " ago"
 }
 
 func ByteCountSI(b int64) string {
@@ -227,7 +345,7 @@ func ByteCountSI(b int64) string {
 		float64(b)/float64(div), "kMGTPE"[exp])
 }
 
-func (r *Result) getSize() string {
+func (r *defaultModel) getSize() string {
 	return ByteCountSI(int64(r.items[r.selected].info.Size()))
 }
 
@@ -274,30 +392,34 @@ func main() {
 		return
 	}
 
-	res := &Result{}
+	res := New()
 
-	err = filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
+	// Evaluate if symlink ~/.kube/config already exists since we don't want to overwrite the users config file
+	if err = CheckConfig(path.Join(p, "config")); errors.Is(err, ErrExist) {
+		res.state = stateShowWarning
+	}
+
+	err = filepath.Walk(p, func(filePath string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			config, err := clientcmd.LoadFromFile(path)
+			config, err := clientcmd.LoadFromFile(filePath)
 			if err != nil {
 				return nil
 			}
-			// Read file so that we can reference raw data later
-			b, err := os.ReadFile(path)
-			if err != nil {
-				return nil
-			}
-			i := Item{
+			i := &Item{
 				config: config,
 				info:   info,
 				title:  info.Name(),
 				desc:   fmt.Sprintf("%d contexts", len(config.Contexts)),
-				raw:    b,
 			}
-			res.Add(i)
+			if pa, err := filepath.EvalSymlinks(path.Join(p, "config")); err == nil {
+				if pa == filePath {
+					i.isSelected = true
+				}
+			}
+			res.defaultModel.Add(i)
 		}
 		return nil
 	})
@@ -305,8 +427,9 @@ func main() {
 		fmt.Printf("%s", err)
 		os.Exit(1)
 	}
-	res.list = list.New(res.List(), list.NewDefaultDelegate(), 0, 0)
-	res.list.Title = fmt.Sprintf("%d kubeconfigs in %s", len(res.items), p)
+
+	res.defaultModel.list = list.New(res.defaultModel.List(), list.NewDefaultDelegate(), 0, 0)
+	res.defaultModel.list.Title = p
 
 	prog := tea.NewProgram(res, tea.WithAltScreen())
 	if _, err := prog.Run(); err != nil {
@@ -314,4 +437,174 @@ func main() {
 		os.Exit(1)
 	}
 
+}
+
+func CheckConfig(p string) error {
+	sPath, err := filepath.EvalSymlinks(p)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	// If target file is same as symlink then it is probably not a symlink
+	if sPath == p {
+		return ErrExist
+	}
+	return nil
+}
+
+var (
+	choices = []string{
+		"Make a backup and create a symlink to it",
+		"Delete it. I want a fresh start",
+		"Do nothing, I'm too paranoid to know what to do",
+	}
+	titleStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#7D56F4")).
+			Margin(1, 2).
+			Width(70)
+	selectedStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("230")).
+			MarginLeft(2)
+	inactiveStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("212")).
+			MarginLeft(2)
+)
+
+type warningModel struct {
+	cursor int
+	choice string
+	errMsg string
+}
+
+func newWarningModel() warningModel {
+	return warningModel{}
+}
+
+func (m warningModel) Init() tea.Cmd {
+	return nil
+}
+
+type OverwriteConfigMsg string
+
+func OverwriteConfigCmd() tea.Cmd {
+	return func() tea.Msg {
+		return OverwriteConfigMsg("Overwriting config")
+	}
+}
+
+func (m warningModel) update(msg tea.Msg) (warningModel, tea.Cmd) {
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+		case "enter":
+			m.choice = choices[m.cursor]
+			err := m.executeChoice(m.cursor)
+			if err != nil {
+				m.errMsg = err.Error()
+			} else {
+				cmds = append(cmds, OverwriteConfigCmd())
+			}
+
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+	}
+	return m, tea.Batch(cmds...)
+}
+
+func (m *warningModel) executeChoice(i int) error {
+	p, err := parseArgs()
+	if err != nil {
+		return err
+	}
+	switch i {
+	case 0:
+		err := m.copyFile(path.Join(p, "config"), path.Join(p, fmt.Sprintf("%s_kubecfg-backup", "config")))
+		if err != nil {
+			return err
+		}
+		err = os.Remove(path.Join(p, "config"))
+		if err != nil {
+			return err
+		}
+		err = m.createLink(fmt.Sprintf("%s_kubecfg-backup", "config"), path.Join(p, "config"))
+		if err != nil {
+			return err
+		}
+	case 1:
+	case 2:
+	default:
+	}
+	return nil
+}
+
+func (m *warningModel) copyFile(src, dst string) error {
+	// srcStat, err := os.Stat(src)
+	// if err != nil {
+	// 	return err
+	// }
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *warningModel) createLink(target, link string) error {
+	return os.Symlink(target, link)
+}
+
+// func (m *warningModel) SwitchConfig(src, dst string) error {
+// 	if err := m.copyFile(src, dst); err != nil {
+// 		return err
+// 	}
+// 	if err :=
+// }
+
+func (m warningModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m.update(msg)
+}
+
+func (m warningModel) View() string {
+	s := strings.Builder{}
+	s.WriteString(titleStyle.Render("Kubecfg uses symbolic links to switch between different kubeconfig files. However, an existing kubeconfig file was found. What do you want me to do with it?") + "\n")
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString(selectedStyle.Render("➜ " + choices[i]))
+		} else {
+			s.WriteString(inactiveStyle.Render("  " + choices[i]))
+		}
+		s.WriteString("\n")
+	}
+
+	if len(m.errMsg) > 0 {
+		s.WriteString("\n  " + errorStyle.Render(fmt.Sprintf("Error: %s", m.errMsg)))
+	}
+
+	return s.String()
 }
---
+++ pkg/view/view.go
@@ -0,0 +1,80 @@
+package view
+
+import (
+	"fmt"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	choices    = []string{"Make a backup and create a symlink to it", "Delete it. I want a fresh start", "Do nothing, I'm too paranoid to know what to do"}
+	titleStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#7D56F4")).
+			Margin(1, 2)
+	selectedStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("230")).
+			MarginLeft(2)
+	inactiveStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("212")).
+			MarginLeft(2)
+)
+
+type OverwriteConfigModel struct {
+	cursor int
+	choice string
+}
+
+func (m OverwriteConfigModel) Init() tea.Cmd {
+	return nil
+}
+
+type OverwriteConfigMsg string
+
+func OverwriteConfigCmd() tea.Cmd {
+	return func() tea.Msg {
+		return OverwriteConfigMsg("Overwriting config")
+	}
+}
+
+func (m OverwriteConfigModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+		case "enter":
+			fmt.Println("enter!")
+			m.choice = choices[m.cursor]
+			return m, OverwriteConfigCmd()
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m OverwriteConfigModel) View() string {
+	s := strings.Builder{}
+	s.WriteString(titleStyle.Render("Kubecfg uses symbolic links to switch between different kubeconfig files. However, an existing kubeconfig file was found. What do you want me to do with it?") + "\n")
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString(selectedStyle.Render("➜ " + choices[i]))
+		} else {
+			s.WriteString(inactiveStyle.Render("  " + choices[i]))
+		}
+		//s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+
+	return s.String()
+}
